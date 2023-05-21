3500a32f008bacd50c07ddb4cbd11b3781e777ef
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-21 12:56:54 +0300

    refactor splash view

--- lib/features/splash/view/splash_view.dart
+++ lib/features/splash/view/splash_view.dart
@@ -1,10 +1,20 @@
+import 'package:act_hub_project/core/resources/manager_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../core/resources/manager_sizes.dart';
+import '../../../core/widgets/scaffold_with_background_image.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return scaffoldWithBackgroundImage(child: Center(
+          child: SvgPicture.asset(
+        ManagerAssets.logo,
+        width: ManagerWidth.w167,
+      )),
+    );
   }
 }
