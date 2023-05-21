64dc6b2e146a62235e58293037fe5648478a2b21
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-21 15:59:01 +0300

    edit manager text theme dark file

--- lib/core/resources/manager_text_theme_dark.dart
+++ lib/core/resources/manager_text_theme_dark.dart
@@ -1,26 +1,3 @@
 import 'package:flutter/material.dart';
-import 'manager_colors.dart';
-import 'manager_fonts.dart';
-import 'manager_styles.dart';
 
-class ManagerTextThemeDark extends TextTheme {
-  @override
-  TextStyle get displayMedium => getMediumStyle(
-      fontSize: ManagerFontSize.s20, color: ManagerColors.textColor);
-
-  @override
-  TextStyle get displaySmall => getBoldStyle(
-      fontSize: ManagerFontSize.s16, color: ManagerColors.textColor);
-
-  @override
-  TextStyle get headlineMedium => getMediumStyle(
-      fontSize: ManagerFontSize.s16, color: ManagerColors.textColor);
-
-  @override
-  TextStyle get headlineSmall => getTextStyle(
-      fontSize: ManagerFontSize.s16, color: ManagerColors.textColor);
-
-  @override
-  TextStyle get titleMedium => getMediumStyle(
-      fontSize: ManagerFontSize.s14, color: ManagerColors.textColor);
-}
+class ManagerTextThemeDark extends TextTheme {}
