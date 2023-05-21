0cbb1b6bb8fe5a92272ede5adf690ee2cebf703d
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-21 15:55:44 +0300

    add elevation value to constants file

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,4 +2,5 @@ class Constants {
   static const double deviceWidth = 375.0;
   static const double deviceHeigth = 812.0;
   static const int splashDuration = 3;
+  static const double elevationAppBar = 0;
 }
