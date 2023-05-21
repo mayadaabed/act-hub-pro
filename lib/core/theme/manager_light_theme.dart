08a02a4644f8932dc38a1a24554e29b61bc44e2a
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-21 15:59:44 +0300

    edit manager ligth theme file

--- lib/core/theme/manager_light_theme.dart
+++ lib/core/theme/manager_light_theme.dart
@@ -1,7 +1,38 @@
+import 'package:act_hub_project/config/constants.dart';
 import 'package:act_hub_project/core/resources/manager_colors.dart';
+import 'package:act_hub_project/core/resources/manager_fonts.dart';
+import 'package:act_hub_project/core/resources/manager_styles.dart';
+import 'package:act_hub_project/core/resources/manager_text_theme_light.dart';
 import 'package:flutter/material.dart';
 
 ThemeData managerLightThemeData() {
-  return ThemeData.light()
-      .copyWith(useMaterial3: true, primaryColor: ManagerColors.primaryColor);
+  return ThemeData.light().copyWith(
+      useMaterial3: true,
+
+      // MAIN COLORs APP
+      primaryColor: ManagerColors.primaryColor,
+      primaryColorLight: ManagerColors.primaryColorLight,
+
+      // disabled button colors
+      disabledColor: ManagerColors.textColor,
+      splashColor: ManagerColors.greyLight,
+      scaffoldBackgroundColor: ManagerColors.scaffoldBackgroundColorLight,
+
+      // APP BAR THEME
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ManagerColors.greyLight,
+        elevation: Constants.elevationAppBar,
+        titleTextStyle: getBoldTextStyle(
+          fontSize: ManagerFontSize.s14,
+          color: ManagerColors.primaryColor,
+        ),
+      ),
+
+      // BUTTON THEME
+      buttonTheme: const ButtonThemeData(
+          shape: RoundedRectangleBorder(),
+          disabledColor: ManagerColors.greyLight,
+          buttonColor: ManagerColors.buttonColorLight),
+      textTheme: ManagerTextThemeLight());
 }
