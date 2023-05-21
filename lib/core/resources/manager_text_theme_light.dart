f92e0f755f55f54ea42f2ebdcf210b7f3137bc2e
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-21 16:00:52 +0300

    edit manager text theme light file

--- lib/core/resources/manager_text_theme_light.dart
+++ lib/core/resources/manager_text_theme_light.dart
@@ -1,6 +1,37 @@
-
+import 'package:act_hub_project/core/resources/manager_colors.dart';
+import 'package:act_hub_project/core/resources/manager_fonts.dart';
 import 'package:flutter/material.dart';
 
-class ManagerTextThemeLight extends TextTheme {
+import 'manager_styles.dart';
 
+class ManagerTextThemeLight extends TextTheme {
+  @override
+  TextStyle get displayMedium => getMediumTextStyle(
+        fontSize: ManagerFontSize.s20,
+        color: ManagerColors.textColor,
+      );
+
+  @override
+  TextStyle get displaySmall => getBoldTextStyle(
+        fontSize: ManagerFontSize.s16,
+        color: ManagerColors.textColor,
+      );
+
+  @override
+  TextStyle get headlineMedium => getMediumTextStyle(
+        fontSize: ManagerFontSize.s16,
+        color: ManagerColors.textColor,
+      );
+
+  @override
+  TextStyle get titleMedium => getMediumTextStyle(
+        fontSize: ManagerFontSize.s14,
+        color: ManagerColors.textColor,
+      );
+
+  @override
+  TextStyle get bodyLarge => getRegularTextStyle(
+        fontSize: ManagerFontSize.s16,
+        color: ManagerColors.textColor,
+      );
 }
