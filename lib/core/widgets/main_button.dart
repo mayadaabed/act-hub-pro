3f13205631a78ed74419befce7be089f5e71c02d
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-23 14:15:20 +0300

    refactor main_button

--- lib/core/widgets/main_button.dart
+++ lib/core/widgets/main_button.dart
@@ -1,5 +1,27 @@
+import 'package:act_hub_project/config/constants.dart';
+import 'package:act_hub_project/core/resources/manager_colors.dart';
+import 'package:act_hub_project/core/resources/manager_sizes.dart';
 import 'package:flutter/material.dart';
 
-Widget mainButton({required Widget child}) {
-  return MaterialButton(onPressed: () {}, child: child);
+Widget mainButton({
+  required Widget child,
+  ShapeBorder? shapeBorder,
+  Color? color,
+  double? minWidth,
+  double? height,
+  double? elevation,
+}) {
+  return MaterialButton(
+      onPressed: () {},
+      shape: shapeBorder ??
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              ManagerRadius.r12,
+            ),
+          ),
+      color: color ?? ManagerColors.transparent,
+      minWidth: minWidth ?? ManagerWidth.w16,
+      height: height ?? ManagerHeight.h16,
+      elevation: elevation ?? Constants.elevationButton,
+      child: child);
 }
