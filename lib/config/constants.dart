730c1faefb8d65d3a693662629f243677af72407
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-23 14:13:44 +0300

    add elevationButton to constants file

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -4,5 +4,7 @@ class Constants {
   static const int splashDuration = 3;
   static const double elevationAppBar = 0;
   static const int outBoardingDuration = 1;
+  static const double elevationButton = 0;
+
 
 }
