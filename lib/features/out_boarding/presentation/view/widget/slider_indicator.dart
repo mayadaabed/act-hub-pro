d596541a9827a083e313415161e6347ddb082f6c
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-23 21:11:25 +0300

    add getBuilder to sliderIndicator

--- lib/features/out_boarding/presentation/view/widget/slider_indicator.dart
+++ lib/features/out_boarding/presentation/view/widget/slider_indicator.dart
@@ -1,4 +1,6 @@
+import 'package:act_hub_project/features/out_boarding/presentation/controller/out_boarding_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../../config/constants.dart';
 import '../../../../../core/resources/manager_colors.dart';
@@ -9,32 +11,34 @@ class SilderIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(horizontal: ManagerWidth.w65),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(ManagerRadius.r12),
-            color: ManagerColors.greyLight),
-        width: double.infinity,
-        height: ManagerHeight.h4,
-        child: Row(
-          children: [
-            ...List.generate(
-              3,
-              (index) => Expanded(
-                child: AnimatedContainer(
-                  duration:
-                      const Duration(seconds: Constants.outBoardingDuration),
-                  curve: Curves.fastLinearToSlowEaseIn,
-                  decoration: BoxDecoration(
-                    color: index == 0
-                        ? ManagerColors.primaryColor
-                        : ManagerColors.greyLight,
-                    borderRadius: BorderRadius.circular(ManagerRadius.r12),
+    return GetBuilder<OutBoardingController>(builder: (controller) {
+      return Container(
+          margin: EdgeInsets.symmetric(horizontal: ManagerWidth.w65),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(ManagerRadius.r12),
+              color: ManagerColors.greyLight),
+          width: double.infinity,
+          height: ManagerHeight.h4,
+          child: Row(
+            children: [
+              ...List.generate(
+                3,
+                (index) => Expanded(
+                  child: AnimatedContainer(
+                    duration:
+                        const Duration(seconds: Constants.outBoardingDuration),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    decoration: BoxDecoration(
+                      color: controller.currentPage == index
+                          ? ManagerColors.primaryColor
+                          : ManagerColors.greyLight,
+                      borderRadius: BorderRadius.circular(ManagerRadius.r12),
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
-        ));
+              )
+            ],
+          ));
+    });
   }
 }
