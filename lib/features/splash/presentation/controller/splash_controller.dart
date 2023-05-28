dcf2b46d545d57291cbb743c79bae69baeb2a06c
Mayada Almobayed <abedmayada4@gmail.com>
2023-05-28 15:56:19 +0300

    initalize _appSettingsSharedPreferences in spalsh

--- lib/features/splash/presentation/controller/splash_controller.dart
+++ lib/features/splash/presentation/controller/splash_controller.dart
@@ -1,13 +1,24 @@
 import 'package:act_hub_project/config/constants.dart';
 import 'package:get/get.dart';
+import '../../../../config/dependency_injection.dart';
+import '../../../../core/storage/local/app_settings_shared_preferences.dart';
 import '../../../../routes/routes.dart';
 
 class SplashController extends GetxController {
+  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
+      instance<AppSettingsSharedPreferences>();
+
   @override
   void onInit() {
     super.onInit();
     Future.delayed(const Duration(seconds: Constants.splashDuration), () {
-      Get.offAllNamed(Routes.outBoardingView);
+      if (_appSettingsSharedPreferences.getOutBoardingViewed()) {
+        Get.offAllNamed(Routes.loginView);
+      } else {
+        Get.offAllNamed(
+          Routes.outBoardingView,
+        );
+      }
     });
   }
 }
