64322de8703a4ec8b72d08e5e6c1f767f664c6b7
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-05 01:19:29 +0300

    refactor login view

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,19 +1,16 @@
-import 'package:act_hub_project/config/constants.dart';
 import 'package:act_hub_project/core/resources/manager_colors.dart';
 import 'package:act_hub_project/core/resources/manager_sizes.dart';
 import 'package:act_hub_project/core/resources/manager_styles.dart';
-import 'package:act_hub_project/core/widgets/scaffold_with_background_image.dart';
 import 'package:act_hub_project/features/auth/presentation/controller/login_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-
-import '../../../../core/resources/manager_assets.dart';
 import '../../../../core/resources/manager_fonts.dart';
 import '../../../../core/resources/manager_strings.dart';
 import '../../../../core/validator/validator.dart';
 import '../../../../core/widgets/base_text_form_feild.dart';
 import '../../../../core/widgets/main_button.dart';
+import '../../../../routes/routes.dart';
+import '../widget/auth_view.dart';
 
 class LoginView extends StatelessWidget {
   final FailedValidator _failedValidator = FailedValidator();
@@ -21,91 +18,116 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return scaffoldWithBackgroundImage(
-        child: Column(
-      children: [
-        Expanded(
-          flex: Constants.loginFirstPartFlex,
-          child: Container(
-            margin: EdgeInsetsDirectional.only(top: ManagerHeight.h12),
-            alignment: Alignment.center,
-            child: SvgPicture.asset(ManagerAssets.logo),
-          ),
-        ),
-        Expanded(
-          flex: Constants.loginSecondPartFlex,
-          child: Container(
-            decoration: BoxDecoration(
-              color: ManagerColors.backgroundForm,
-              borderRadius: BorderRadiusDirectional.only(
-                topStart: Radius.circular(
-                  ManagerRadius.r44,
+    return authView(
+      child: GetBuilder<LoginController>(
+        builder: (controller) {
+          return Form(
+            key: controller.formKey,
+            child: Column(
+              children: [
+                Text(
+                  ManagerStrings.login,
+                  style: getMediumTextStyle(
+                    fontSize: ManagerFontSize.s24,
+                    color: ManagerColors.black,
+                  ),
                 ),
-              ),
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: ManagerWidth.w16,
-              vertical: ManagerHeight.h30,
-            ),
-            width: double.infinity,
-            child: GetBuilder<LoginController>(
-              builder: (controller) {
-                return Form(
-                  key: controller.formKey,
-                  child: Column(
-                    children: [
-                      Text(
-                        ManagerStrings.login,
-                        style: getMediumTextStyle(
-                          fontSize: ManagerFontSize.s24,
-                          color: ManagerColors.black,
+                SizedBox(height: ManagerHeight.h30),
+                baseTextFormField(
+                  controller: controller.email,
+                  hintText: ManagerStrings.email,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) => _failedValidator.validateEmail(
+                    value,
+                  ),
+                ),
+                SizedBox(height: ManagerHeight.h16),
+                baseTextFormField(
+                  controller: controller.password,
+                  hintText: ManagerStrings.password,
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  validator: (value) => _failedValidator.validatePassword(
+                    value,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: true,
+                          onChanged: (value) {},
+                          activeColor: ManagerColors.primaryColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(ManagerRadius.r4)),
                         ),
-                      ),
-                      SizedBox(height: ManagerHeight.h30),
-                      baseTextFormField(
-                        controller: controller.email,
-                        hintText: ManagerStrings.email,
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value) => _failedValidator.validateEmail(
-                          value,
-                        ),
-                      ),
-                      SizedBox(height: ManagerHeight.h30),
-                      baseTextFormField(
-                        controller: controller.password,
-                        hintText: ManagerStrings.password,
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                        validator: (value) => _failedValidator.validatePassword(
-                          value,
-                        ),
-                      ),
-                      SizedBox(height: ManagerHeight.h40),
-                      mainButton(
-                        child: Text(
-                          ManagerStrings.login,
-                          style: getRegularTextStyle(
-                            fontSize: ManagerFontSize.s16,
-                            color: ManagerColors.white,
+                        Text(
+                          ManagerStrings.rememberMe,
+                          style: getMediumTextStyle(
+                            fontSize: ManagerFontSize.s14,
+                            color: ManagerColors.black,
                           ),
                         ),
-                        minWidth: double.infinity,
-                        color: ManagerColors.primaryColor,
-                        height: ManagerHeight.h40,
-                        onPressed: () {
-                          if (controller.formKey.currentState!.validate()) {
-                            controller.login(context);
-                          }
-                        },
-                      ),
-                    ],
+                      ],
+                    ),
+                  ],
+                ),
+                mainButton(
+                  child: Text(
+                    ManagerStrings.forgotPassword,
+                    style: getRegularTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.primaryColor),
                   ),
-                );
-              },
+                ),
+                SizedBox(height: ManagerHeight.h100),
+                mainButton(
+                  child: Text(
+                    ManagerStrings.login,
+                    style: getRegularTextStyle(
+                      fontSize: ManagerFontSize.s16,
+                      color: ManagerColors.white,
+                    ),
+                  ),
+                  minWidth: double.infinity,
+                  color: ManagerColors.primaryColor,
+                  height: ManagerHeight.h40,
+                  onPressed: () {
+                    if (controller.formKey.currentState!.validate()) {
+                      controller.login(context);
+                    }
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      ManagerStrings.haveNotAccount,
+                      style: getRegularTextStyle(
+                          fontSize: ManagerFontSize.s14,
+                          color: ManagerColors.black),
+                    ),
+                    mainButton(
+                        child: Text(
+                          ManagerStrings.signUp,
+                          style: getRegularTextStyle(
+                            fontSize: ManagerFontSize.s14,
+                            color: ManagerColors.primaryColor,
+                          ),
+                        ),
+                        onPressed: () {
+                          Get.offAllNamed(Routes.registerView);
+                        }),
+                  ],
+                ),
+              ],
             ),
-          ),
-        ),
-      ],
-    ));
+          );
+        },
+      ),
+    );
   }
 }
