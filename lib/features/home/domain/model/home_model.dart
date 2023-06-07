e6afcae15ec6073a60f2d1f22fc6a00f3f610d64
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-07 17:11:38 +0300

    create HomeModel

---
+++ lib/features/home/domain/model/home_model.dart
@@ -0,0 +1,17 @@
+import '../../../../core/base_model/base_model.dart';
+import 'category_model.dart';
+import 'course_model.dart';
+import 'slider_model.dart';
+
+class HomeModel extends BaseModel {
+  List<SliderModel>? sliders;
+  List<CategoryModel>? categories;
+  List<CourseModel>? courses;
+
+  HomeModel({
+    this.sliders,
+    this.categories,
+    this.courses,
+    super.message,
+  });
+}
