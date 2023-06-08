d97ec6c8d2284c73810ee3caacc0146194d8bb27
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-09 00:55:05 +0300

    add emailVerification path

--- lib/config/request_constants.dart
+++ lib/config/request_constants.dart
@@ -4,4 +4,5 @@ class RequestConstants {
   static const String login = '$basePath/login';
   static const String register = '$basePath/Register';
   static const String home = '$basePath/home';
+  static const String emailVerification = '$basePath/email-verification';
 }
