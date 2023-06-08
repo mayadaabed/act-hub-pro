e5962a561b67eb356885e3e72a22f76077d1a927
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-08 16:45:34 +0300

    create _homeUseCase and edit home

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -1,9 +1,13 @@
 import 'package:carousel_slider/carousel_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_state_render_dialog/flutter_state_render_dialog.dart';
 import 'package:get/get.dart';
 
+import '../../../../config/dependency_injection.dart';
 import '../../domain/model/category_model.dart';
 import '../../domain/model/course_model.dart';
 import '../../domain/model/slider_model.dart';
+import '../../domain/use_case/home_use_case.dart';
 
 class HomeController extends GetxController {
   late CarouselController carouselController;
@@ -15,6 +19,8 @@ class HomeController extends GetxController {
   List<CourseModel> popularCourses = [];
 
   List<CategoryModel> categories = [];
+
+  final HomeUseCase _homeUseCase = instance<HomeUseCase>();
 
   @override
   void onInit() {
@@ -33,7 +39,22 @@ class HomeController extends GetxController {
     update();
   }
 
-  Future<void> home() async {}
+  Future<void> home() async {
+    BuildContext context = Get.context!;
+    (await _homeUseCase.execute()).fold((l) {
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpErrorState,
+        message: l.message,
+        title: '',
+      );
+    }, (r) {
+      sliders = r.sliders!;
+      categories = r.categories!;
+      popularCourses = r.courses!;
+      update();
+    });
+  }
 
   courseHoursFormat(int index) {
     return "${popularCourses[index].attributes!.hours.toString()} hour";
