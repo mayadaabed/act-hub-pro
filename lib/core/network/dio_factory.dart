88880dea29910541f4bfb359ee03e71b9810dd6b
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-11 22:39:31 +0300

    refactor

--- lib/core/network/dio_factory.dart
+++ lib/core/network/dio_factory.dart
@@ -16,6 +16,8 @@ class DioFactory {
     Map<String, String> headers = {
       ApiConstants.authorization:
           getAuthorization(_appSettingsSharedPreferences.getToken()),
+      ApiConstants.accept: ApiConstants.applicationJson,
+      ApiConstants.contentType: ApiConstants.applicationJson,
       ApiConstants.acceptLanguage: ApiConstants.english,
     };
     dio.options = BaseOptions(
