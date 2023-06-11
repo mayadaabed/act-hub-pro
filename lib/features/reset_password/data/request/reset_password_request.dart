97a16e0ed5cfd782e871a054a84fae9ccad1090c
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-11 22:48:53 +0300

    create ResetPasswordRequest

---
+++ lib/features/reset_password/data/request/reset_password_request.dart
@@ -0,0 +1,12 @@
+
+class ResetPasswordRequest {
+  String email;
+  String password;
+  String otp;
+
+  ResetPasswordRequest({
+   required this.email,
+   required this.password,
+    required this.otp,
+  });
+}
