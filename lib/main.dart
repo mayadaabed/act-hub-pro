4160baf386be8e497368629e2b4ba2e2df6090f1
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-25 23:54:26 +0300

    add localizations settings in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:act_hub_project/config/constants.dart';
 import 'package:act_hub_project/core/service/theme_service.dart';
 import 'package:act_hub_project/routes/routes.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
 import 'config/dependency_injection.dart';
+import 'config/localizations/locale_settings.dart';
+import 'core/resources/manager_assets.dart';
 
 void main() async {
   await initModule();
-  runApp(MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: localeSettings.supportedLocales,
+      path: translationPath,
+      fallbackLocale: localeSettings.defaultLocale,
+      startLocale: localeSettings.defaultLocale,
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -25,6 +35,9 @@ class MyApp extends StatelessWidget {
         designSize: const Size(Constants.deviceWidth, Constants.deviceHeigth),
         builder: (context, child) {
           return GetMaterialApp(
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
             debugShowCheckedModeBanner: false,
             onGenerateRoute: RouteGenerator.getRoute,
             initialRoute: Routes.splashView,
