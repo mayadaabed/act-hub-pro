f677c2135dc85e4070b7acd9a65833607a2af499
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-26 00:03:12 +0300

    add editPassword request

--- lib/core/network/app_api.g.dart
+++ lib/core/network/app_api.g.dart
@@ -238,6 +238,35 @@ class _AppApi implements AppApi {
     return value;
   }
 
+  @override
+  Future<EditPasswordResponse> editPassword(
+    dynamic password,
+    dynamic passwordConfirmation,
+  ) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = {
+      'password': password,
+      'password_confirmation': passwordConfirmation,
+    };
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<EditPasswordResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/api/auth/user/edit-user-password',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = EditPasswordResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
